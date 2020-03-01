dabe51054f33785f5d9f8bd3cf8d619961c14041
Karen Kua <karen.kua@points.com>
2020-03-01 17:12:47 -0500

    Set up basic mux router with json encoding and decoding

---
+++ http-api.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Item is a struct that groups all necessary fields into a single unit
+type Item struct {
+	Data      string `json:"data"`
+	OtherData int    `json:"otherData"`
+}
+
+var data []Item = []Item{}
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/add", addItem).Methods("POST")
+
+	http.ListenAndServe(":5000", router)
+}
+
+func addItem(w http.ResponseWriter, r *http.Request) {
+	var newItem Item
+	json.NewDecoder(r.Body).Decode(&newItem)
+	data = append(data, newItem)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(data)
+}
