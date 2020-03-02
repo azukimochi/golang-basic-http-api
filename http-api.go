2ec8b2cc1166db32a8a06ddb685bb6d40eb37ae6
Karen Kua <karen.kua@points.com>
2020-03-01 19:44:16 -0500

    Created POST, GET, PUT, PATCH, and DELETE methods

--- http-api.go
+++ http-api.go
@@ -3,31 +3,140 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
-// Item is a struct that groups all necessary fields into a single unit
-type Item struct {
-	Data      string `json:"data"`
-	OtherData int    `json:"otherData"`
+// Post is a struct that represents a single post
+type Post struct {
+	Title  string `json:"title"`
+	Body   string `json:"body"`
+	Author User   `json:"author"`
 }
 
-var data []Item = []Item{}
+// User is a struct that represents a user in our app
+type User struct {
+	FullName string `json:"fullName"`
+	UserName string `json:"userName"`
+	Email    string `json:"email"`
+}
+
+var posts []Post = []Post{}
 
 func main() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/add", addItem).Methods("POST")
+	router.HandleFunc("/posts", addPost).Methods("POST")
+	router.HandleFunc("/posts", getAllPosts).Methods("GET")
+	router.HandleFunc("/posts/{id}", getPost).Methods("GET")
+	router.HandleFunc("/posts/{id}", updatePost).Methods("PUT")
+	router.HandleFunc("/posts/{id}", patchPost).Methods("PATCH")
+	router.HandleFunc("/posts/{id}", deletePost).Methods("DELETE")
 
 	http.ListenAndServe(":5000", router)
 }
 
-func addItem(w http.ResponseWriter, r *http.Request) {
-	var newItem Item
-	json.NewDecoder(r.Body).Decode(&newItem)
-	data = append(data, newItem)
+func getPost(w http.ResponseWriter, r *http.Request) {
+	var idParam string = mux.Vars(r)["id"]
+	id, err := strconv.Atoi(idParam)
+
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("ID could not be converted to integer"))
+		return
+	}
+
+	if id >= len(posts) {
+		w.WriteHeader(404)
+		w.Write([]byte("No post found with specified id"))
+		return
+	}
+
+	post := posts[id]
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(post)
+}
+
+func getAllPosts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(posts)
+}
+
+func addPost(w http.ResponseWriter, r *http.Request) {
+	var newPost Post
+	json.NewDecoder(r.Body).Decode(&newPost)
+	posts = append(posts, newPost)
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(data)
+	json.NewEncoder(w).Encode(posts)
+}
+
+func updatePost(w http.ResponseWriter, r *http.Request) {
+	var idParam string = mux.Vars(r)["id"]
+	id, err := strconv.Atoi(idParam)
+
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("ID could not be converted into an integer"))
+		return
+	}
+
+	if id > len(posts) {
+		w.WriteHeader(404)
+		w.Write([]byte("No post found with specified id"))
+		return
+	}
+
+	var updatedPost Post
+	json.NewDecoder(r.Body).Decode(&updatedPost)
+	posts[id] = updatedPost
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(updatedPost)
+}
+
+func patchPost(w http.ResponseWriter, r *http.Request) {
+	var idParam string = mux.Vars(r)["id"]
+	id, err := strconv.Atoi(idParam)
+
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("ID could not be converted into an integer"))
+		return
+	}
+
+	if id > len(posts) {
+		w.WriteHeader(404)
+		w.Write([]byte("No post found with specified id"))
+		return
+	}
+
+	post := &posts[id]
+	json.NewDecoder(r.Body).Decode(post)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(post)
+}
+
+func deletePost(w http.ResponseWriter, r *http.Request) {
+	var idParam string = mux.Vars(r)["id"]
+	id, err := strconv.Atoi(idParam)
+
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("ID could not be converted into an integer"))
+		return
+	}
+
+	if id > len(posts) {
+		w.WriteHeader(404)
+		w.Write([]byte("No post found with specified id"))
+		return
+	}
+	// Delete the post from the slice. This is from the wiki for slice tricks, as slice doesn't have a delete method
+	// https://github.com/golang/go/wiki/SliceTricks
+	posts = append(posts[:id], posts[id+1:]...)
+
+	w.WriteHeader(200)
 }
